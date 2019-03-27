37bcd625f134ca7d59c82d4f2c822b2542736b9d
= <=>
2019-03-27 11:44:19 -0400

    Generate activities and labels for cohort context

---
+++ lib/relearn_together/cohorts/activity.ex
@@ -0,0 +1,19 @@
+defmodule RelearnTogether.Cohorts.Activity do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "activities" do
+    field :cohort_id, :id
+    field :mod_id, :id
+    field :label_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(activity, attrs) do
+    activity
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
