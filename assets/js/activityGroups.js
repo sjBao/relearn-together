619f9c9571400567579e16b5f0b531e0d0231561
= <=>
2019-03-28 16:18:04 -0400

    Adds ability to dynamically create new group divs

--- assets/js/activityGroups.js
+++ assets/js/activityGroups.js
@@ -1,5 +1,35 @@
-export var Groupings = {
+export const Groupings = {
   run: () => {
-    console.log('hello');
+    const newGroupButton = document.querySelector('#new-activity-group');
+    const groupsContainer = document.querySelector('#group-maker-container');
+    newGroupButton.addEventListener('click', event => {
+      groupsContainer.lastElementChild.before(Group().render());
+    })
   }
+}
+
+
+const Group = () => {
+  const node = document.createElement('template');
+  const render = () => {
+    node.innerHTML = `
+<div class="group-card uk-width-1-3">
+  <h4>Group 1</h4>
+  <div uk-sortable="group: sortable-group" class="groupings">
+  </div>
+</div>`;
+    return node.content;
+  }
+
+  return {
+    render
+  }
+}
+
+const Student = () => {
+  const render = () => `
+    <div class="sortable-student-card">
+      <div class="">Item 1</div>
+    </div>
+  `
 }
--- lib/relearn_together_web/views/activity_view.ex
+++ lib/relearn_together_web/views/activity_view.ex
@@ -1,10 +1,3 @@
 defmodule RelearnTogetherWeb.ActivityView do
   use RelearnTogetherWeb, :view
-
-  def render("scripts.html", _assigns) do
-    ~s{
-      <script>
-      </script>
-    } |> raw
-  end
 end
